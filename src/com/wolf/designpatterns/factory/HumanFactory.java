8d81b6e5c0f30d6c1789b28a57c0598b92dba09b
WolfLB <sunboy_liaobin20@163.com>
2016-05-08 21:08:30 +0800

    工厂模式

--- src/com/wolf/designpatterns/factory/HumanFactory.java
+++ src/com/wolf/designpatterns/factory/HumanFactory.java
@@ -1,5 +1,6 @@
 package com.wolf.designpatterns.factory;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -9,7 +10,6 @@ import java.util.Random;
 public class HumanFactory {
 
     /**
-     *
      * @param z
      * @return
      */
@@ -33,7 +33,32 @@ public class HumanFactory {
         Random random = new Random();
         int rand = random.nextInt(concreteHumanList.size());
 
-//        human = createHuman(concreteHumanList.get(rand));
+        human = createHuman(concreteHumanList.get(rand));
+        return human;
+    }
+
+    /**
+     * 延迟加载
+     */
+    private static HashMap<String, IHuman> humans = new HashMap<String, IHuman>();
+
+    public static IHuman lazyCreateHuman(Class c) {
+        IHuman human = null;
+        try {
+            if (humans.containsKey(c.getSimpleName())) {
+                human = humans.get(c.getSimpleName());
+            } else {
+                human = (IHuman) Class.forName(c.getName()).newInstance();
+                //放到MAP中
+                humans.put(c.getSimpleName(), human);
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return human;
     }
 }
